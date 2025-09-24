836e113a18eb483d7d6f26135d979afb0b2bc4de
Toufiq Shishir <toufiq_a24@outlook.com>
2025-09-24 10:02:05 +0600

    chore: update dependencies in pnpm-lock.yaml and restructure IdeasModule imports

--- api/src/ideas/ideas.module.ts
+++ api/src/ideas/ideas.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+
 import { IdeasService } from './ideas.service';
 import { IdeasController } from './ideas.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Idea } from './entities/idea.entity';
+import { IdeaModel } from './entities/idea-model.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Idea, IdeaModel]), AuthModule],
   providers: [IdeasService],
   controllers: [IdeasController],
   exports: [IdeasService],
