7b6408fb0d14127978b2454e1bcfd77ae360c85b
Toufiq Shishir <toufiq_a24@outlook.com>
2025-09-24 10:32:41 +0600

    feat: add Swagger support for API documentation and update package dependencies

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const options = new DocumentBuilder()
+    .setTitle('BizPilot API')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('tags')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
